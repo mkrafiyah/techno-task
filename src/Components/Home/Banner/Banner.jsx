bcaf254d48b28251af86a1e08c3695f25c9210d3
mkrafiyah <rafiyah.mahbub@gmail.com>
2023-12-22 15:13:25 +0400

    banner image edited

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -11,7 +11,7 @@ const Banner = () => {
     return (
         <div className="hero min-h-screen bg-blue-100">
         <div  className="hero-content flex-col lg:flex-row">
-          <img data-aos='fade-up'  src= {bannerImg}className=" w-96 lg:max-w-2xl rounded-lg shadow-2xl" />
+          <img data-aos='fade-up'  src= {bannerImg}className=" w-96 lg:w-[700px] rounded-lg shadow-2xl" />
           <div  data-aos='fade-up'>
             <h1 data-aos='fade-down'  className="text-5xl font-bold text-sky-900">Manage Your Tasks!!!</h1>
             <p data-aos='fade-up' className="py-6">Manage simple to complex projects with us with less efforts.</p>
